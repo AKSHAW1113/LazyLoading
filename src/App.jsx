da42957ecc87f633d958156ed7150eb7ab6d81ec
Anand6262 <pyexpert62@gmail.com>
2026-08-21 11:10:47 +0530

    lazy loading

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+// // 1) Normal code => [INSPECT => LIGHTHOUSE SCORE 76%]
+// import React, { useState } from 'react'
+// import User from './User'
+
+// function App() {
+// const [load, setLoad] = useState(false)
+//   return (
+//     <div>
+//       <h1>Lazy loading</h1>
+//       <button onClick={() => setLoad(true)}>Load user</button>
+//       {load ? <User /> : null}
+//     </div>
+//   )
+// }
+// export default App
+
+// **2) Performance optmized code! => [INSPECT => LIGHTHOUSE SCORE 82%]
+// Note: We can write lazy() or, React.lazy()
+import React, { lazy, Suspense, useState } from 'react'
+const User = lazy(() => import('./User'))
+
+function App() {
+const [load, setLoad] = useState(false)
+  return (
+    <div>
+      <h1>Lazy loading</h1>
+      <button onClick={() => setLoad(true)}>Load user</button>
+      {load ? <Suspense fallback={<h3>loding...</h3>}><User /></Suspense> : null}
+
+    </div>
+  )
+}
+export default App
